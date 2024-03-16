dd426cd95a648b1461b421959115472fa4416c05
Mariano Masondo <nanomason@gmail.com>
2024-03-16 18:35:25 -0300

    Pages

--- src/models/task.class.js
+++ src/models/task.class.js
@@ -1,15 +1,15 @@
 import { LEVELS } from "./levels.enum";
 
 export class Task {
-    name= "";
-    description= "";
-    comleted= false;
-    level= LEVELS.NORMAL;
+  name = "";
+  description = "";
+  comleted = false;
+  level = LEVELS.NORMAL;
 
-    constructor(name, description, comleted, level){
-        this.name = name;
-        this.description = description;
-        this.comleted = comleted;
-        this.level = level;
-    }
+  constructor(name, description, comleted, level) {
+    this.name = name;
+    this.description = description;
+    this.comleted = comleted;
+    this.level = level;
+  }
 }
