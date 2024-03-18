01a98ffcd2479268429c091913005f4e168e8f99
Mariano Masondo <nanomason@gmail.com>
2024-03-18 19:11:51 -0300

    App2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         {/* <Greeting name="Mariano"></Greeting> */}
         {/*Componente de ejemplo funcional*/}
         {/*<GreetingFunctionalComponent name="Mariano"></GreetingFunctionalComponent>*/}
-        
         {/*Componente de listado de tareas*/}
         <TaskListComponent></TaskListComponent>
       </header>
