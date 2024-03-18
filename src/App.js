ce1009d823431da80843835a9cc7522149d7e65f
Mariano Masondo <nanomason@gmail.com>
2024-03-18 18:46:38 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // import GreetingFunctionalComponent from './components/pure/greetingFunctional Component';
-import TaskListComponent from './components/container/task_list';
+import TaskListComponent from "./components/container/task_list";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
         {/* <Greeting name="Mariano"></Greeting> */}
         {/*Componente de ejemplo funcional*/}
         {/*<GreetingFunctionalComponent name="Mariano"></GreetingFunctionalComponent>*/}
+        
         {/*Componente de listado de tareas*/}
         <TaskListComponent></TaskListComponent>
-
       </header>
     </div>
   );
