c6b8e670909716a3336c6abfd7f2d17a6a96b5ad
Mariano Masondo <nanomason@gmail.com>
2024-03-24 12:46:33 -0300

    props.children ejemplo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TaskListComponent from "./components/container/task_list";
 import Ejemplo1 from "./hooks/Ejemplo1";
 import Ejemplo2 from "./hooks/Ejemplo2";
 import MiComponenteConContexto from "./hooks/Ejemplo3";
+import Ejemplo4 from "./hooks/Ejemplo4";
 
 function App() {
   return (
@@ -20,7 +21,13 @@ function App() {
         {/*Ejemplo de susu de hooks*/}
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
-        <MiComponenteConContexto></MiComponenteConContexto>
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        <Ejemplo4 nombre="Martín">
+        {/* Todo lo que hay aquí dentro es tratado como props.children */}
+          <h3>
+            Contenido de props.children
+          </h3>
+        </Ejemplo4>
       </header>
     </div>
   );
