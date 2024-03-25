64dbf85162f346e040db2129e0597c2afd5b62ee
Mariano Masondo <nanomason@gmail.com>
2024-03-25 15:44:16 -0300

    Life cicle

---
+++ src/components/pure/lifeCicleExalmple.jsx
@@ -0,0 +1,62 @@
+// Ejemplo de componente tipo clase que dispone de los métodos de ciclo de vida
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class lifeCicleExample extends Component {
+    constructor(props) {
+        super(props)
+        console.log('CONSTRUCTOR: Cuando se instancia el componente')
+
+    }
+
+    componentWillMount() {
+        console.log('WILLMOUNT: Antes del montaje del componente')
+
+    }
+
+    componentDidMount() {
+        console.log('DIDMOUNT: Justo al acabar el montaje del componente antes de renderizarlo')
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('WillReciebeProps: Si va a recibir nuevas props')
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        //Controla si el componente ebe o no actulizarse
+        //return true /false
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('WillUpdate: Justo antes de actualizarse')
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('DidUpdate: Justo después de actualizarse')
+
+    }
+
+    componentWillUnmount() {
+        console.log('WillUnmount: Justo antes de desaparecere')
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+componentName.propTypes = {
+
+}
+
+export default componentName
