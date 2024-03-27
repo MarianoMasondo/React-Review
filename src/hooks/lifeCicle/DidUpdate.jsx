66f188ade882221199658780a4b559b424f6379f
Mariano Masondo <nanomason@gmail.com>
2024-03-27 18:50:57 -0300

    LifeCicle didUpdate componete funcional

--- src/hooks/lifeCicle/DidUpdate.jsx
+++ src/hooks/lifeCicle/DidUpdate.jsx
@@ -1,6 +1,6 @@
 // Ejemplo de uso demétodo componenteDidUpdate en componete clase y uso de Hook en componente funcional.
 
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 
 export class DidUpdate extends Component {
     componentDidUpdate(){
@@ -16,9 +16,12 @@ export class DidUpdate extends Component {
 }
 
 export const DidUpdateHook = () => {
+    useEffect(() => {
+        console.log('Comportamiento cuando el componente recibenuevos props o cambios en su estado privado')
+    });
     return (
         <div>
-            
+            <h1>DidUpdate</h1>            
         </div>
     );
 }
