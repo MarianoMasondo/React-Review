b8e01f24edaea4f46fad843c3aec4543da4732fe
Mariano Masondo <nanomason@gmail.com>
2024-03-27 18:44:14 -0300

    LifeCicle Hook en componete funcional

--- src/hooks/lifeCicle/DidMount.jsx
+++ src/hooks/lifeCicle/DidMount.jsx
@@ -1,6 +1,7 @@
-// Ejemplo de uso del método de ciclo de vida encomponente de clase y el hook de ciclo de vida encomponente funcional.
+// Ejemplo de uso del método de ciclo de vida en componente de clase y el hook de ciclo de vida encomponente funcional.
 
 import React, { Component } from 'react';
+import { useEffect } from 'react';
 
 export class DidMount extends Component {
     componenteDidMount(){
@@ -10,7 +11,7 @@ export class DidMount extends Component {
     render() {
         return (
             <div>
-            <h1>DidMount</h1>                
+                <h1>DidMount</h1>                
             </div>
         );
     }
@@ -18,11 +19,12 @@ export class DidMount extends Component {
 
 
 export const DidMountHook = () => {
+    useEffect(() => {
+        console.log('Comportamiento antes de que el componente sea añadido al DOM (renderice)')
+    }, [])
     return (
         <div>
-            
+            <h1>DidMount</h1>            
         </div>
     );
 }
-
-export default DidMount;
