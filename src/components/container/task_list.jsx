76fd6f88cb32709d75394ea66cbbbdc01d2e46df
Mariano Masondo <nanomason@gmail.com>
2024-03-27 19:57:53 -0300

    Task component useEffect

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -14,11 +14,12 @@ const TaskListComponent = () => {
     
     // Estado del componente
   const [tasks, setTasks] = useState([defaultTask]);
+  const [loading, setLoading] = useState(true);
 
   // Control del ciclo de vida del componmente
-
   useEffect(() => {
     console.log('Task State has been modified');
+    setLoading(false)
     return () => {
       console.log('TaskList component is going to unmount')
     }
