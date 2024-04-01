7c6731dce52d26e393b6cd410e839bc84e4ab7e8
Mariano Masondo <nanomason@gmail.com>
2024-04-01 11:23:24 -0300

    Badgen

--- src/components/pure/task.jsx
+++ src/components/pure/task.jsx
@@ -5,6 +5,7 @@ import { Task } from "../../models/task.class";
 
 //Importamos la hoja de estilo de task.scss
 import '../../styles/task.scss'
+import { LEVELS } from "../../models/levels.enum";
 
 const TaskComponent = ({ task }) => {
 
@@ -15,6 +16,30 @@ const TaskComponent = ({ task }) => {
     };
   }, [task]);
 
+  // Function thar returns a Badge depending on the level of the task.
+
+  function taskLevelBadge(){
+    switch (task.level) {
+      case LEVELS.NORMAL:        
+        return(<h6 className="mb-0"><span className="badge bg-primary">{task.level}</span></h6>)
+      case LEVELS.URGENT:        
+        return(<h6 className="mb-0"><span className="badge bg-warning">{task.level}</span></h6>)
+      case LEVELS.BLOCKING:        
+        return(<h6 className="mb-0"><span className="badge bg-danger">{task.level}</span></h6>)    
+      default:
+        break;
+    }
+  }
+
+  // Function thar returns icon depending complection of the task
+  function taskComletedIcon(){
+    if(task.completed){
+      return (<i className="bi-toggle-on" style={{color:"green"}}></i>)
+    }else{
+      return (<i className="bi-toggle-off" style={{color: "grey"}}></i>)
+    }
+  }
+
   return (
     <tr className="fw-normal">
       <th>
@@ -24,15 +49,12 @@ const TaskComponent = ({ task }) => {
       <span className="align-middle">{task.description}</span>
       </td>
       <td>
-      {/* TODO: Sustituir por un badge */}
-      <span className="align-middle">{task.level}</span>
+      {/* TExcecution of function to return badge element */}
+      <span className="align-middle">{taskLevelBadge()}</span>
       </td>
       <td>
-        {task.completed ? 
-        ( <i className="bi-toggle-on" style={{ color: "green" }}></i>
-        ) : (
-          <i className="bi-toggle-off" style={{ color: "grey" }}></i>
-        )}
+      {/* Excecution of function to return badge element */}
+        {taskComletedIcon()}
         <i className="bi-trash" style={{ color: "tomato"}}></i>
       </td>
     </tr>
