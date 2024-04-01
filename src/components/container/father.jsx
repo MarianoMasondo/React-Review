281899aaa26630c1289ecd0650ff02fafe12a513
Mariano Masondo <nanomason@gmail.com>
2024-04-01 13:18:10 -0300

    Child to Father

--- src/components/container/father.jsx
+++ src/components/container/father.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Child from '../pure/child';
 
 const Father = () => {
+
+    const [name, setName] = useState("Marian");
+
+    function showMessage(text){
+        alert(`Message received: ${text}`)
+    }
+
+    function updateName(newName){
+        setName(newName)
+        
+    }
+
     return (
-        <div>
-            <Child></Child>
+        <div style={{background: "tomato", padding: "10px"}}>
+            <Child name={name} send={showMessage} update={updateName}></Child>
         </div>
     );
 }
--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const Child = ({name}) => {
+const Child = ({name, send, update}) => {
+
+    const messageRef = useRef("")
+    const nameRef = useRef("")
 
     function pressButton(){
-        alert(`Default Text`);        
+        const text = messageRef.current.value
+        alert(`Text in Input: ${text}`);        
     }
 
     function pressButtonParams(text){
         alert(`TEXT: ${text}`);
     }
 
+    function submitName(e){
+        e.preventDefault();
+        update(nameRef.current.value)
+    }
+
     return (
-        <div>
-            <p onMouseOver={() => console.log("OnMouse Over")}>Child Component</p>
+        <div style={{background: "cyan", padding:"30px"}}>
+            <p onMouseOver={() => console.log("OnMouse Over")}>Hello, {name}</p>
             <button onClick={() => console.log("Pressed Button 1")}>Button 1</button>
             <button onClick={pressButton}>Button 2</button>
             <button onClick={() => pressButtonParams("Hello!")}>Button 3</button>
-            <input placeholder='Insert a text' onFocus={() => console.log("Input Fucused")} onChange={(e) => console.log("Input changed", e.target.value)} onCopy={() => console.log("Copied text from Input")} />
+            <input placeholder='Send a text to your father' onFocus={() => console.log("Input Fucused")} onChange={(e) => console.log("Input changed", e.target.value)} onCopy={() => console.log("Copied text from Input")} ref={messageRef}/>
+            <button onClick={() => send(messageRef.current.value)}>Send Message</button>
+            <div style={{marginTop: "20px"}}>
+                <form onSubmit={submitName}>
+                    <input ref={nameRef} placeholder='New Name' />
+                    <button type='submit'>Update Name</button>
+                </form>
+            </div>
         </div>
     );
 }
