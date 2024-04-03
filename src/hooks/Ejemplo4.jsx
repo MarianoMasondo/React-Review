48cf5d03a9ae9000f2a0c07d83592548701e6913
Mariano Masondo <nanomason@gmail.com>
2024-04-03 20:24:02 -0300

    Ejemplo 4 format

--- src/hooks/Ejemplo4.jsx
+++ src/hooks/Ejemplo4.jsx
@@ -5,10 +5,13 @@ const Ejemplo4 = (props) => {
     return (
         <div>
             <h1>*** Ejemplo de CHILDREN PROPS ***</h1>
+
             <h2>
                 Nombre: {props.nombre}
             </h2>
+
             {/* props.children pintará por defecto aquello que se encuentre entre las etiquetas de apertura y cierre de este componente desde el componente de orden superior */}
+            
             {props.children}
         </div>
     );
